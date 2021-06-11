553e8411e7a46c64dcbca08dbcf442a1d33d6c32
saurabh <saurabh.9901s@gmail.com>
2021-06-11 22:54:00 +0530

    fix toggle script

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function toggleSkills(){
     for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
     }
-    if(itemClass === 'skills__content skills__class'){
+    if(itemClass === 'skills__content skills__close'){
         this.parentNode.className = 'skills__content skills__open' 
     }
 }
