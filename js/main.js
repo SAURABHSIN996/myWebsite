1133a85066be924985c428112ca5592a15d309d2
saurabh <saurabh.9901s@gmail.com>
2021-06-11 21:48:06 +0530

    added skills section

--- js/main.js
+++ js/main.js
@@ -32,6 +32,12 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content')
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass
+}
 
 /*==================== QUALIFICATION TABS ====================*/
 
